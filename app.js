bcb73dc8ab4d6277c4fbdede3ac62e617955de1d
Marcelo de Oliveira Carneiro <marcelo.oliveira.carneiro@gmail>
2024-12-02 14:55:23 -0300

    Revert "Alterei para 30"
    This reverts commit ec2902c6f87012f0a9b84840c43932d41d1adc4f.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let listaDeNumerosSorteados = [];
+<<<<<<< HEAD
 let numeroLimite = 40;
+=======
+let numeroLimite = 50;
+>>>>>>> parent of ec2902c (Alterei para 30)
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
